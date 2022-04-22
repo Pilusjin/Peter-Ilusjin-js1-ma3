c6cca9c236a5854498bf0597dcde51ea603c7961
Pilusjin <75672659+Ilusjin@users.noreply.github.com>
2022-04-22 20:51:27 +0200

    summary
    Finishing project

--- js/question-2.js
+++ js/question-2.js
@@ -1,29 +1,38 @@
 const url ="https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=4c132ca6e34e46b88149c5bd76322d9c";
-const proxy = "https://noroffcors.herokuapp.com/";
 
-const corsFix =proxy + url;
-
-const resultContainer = document.querySelector(".results");
+const gamesections =document.querySelector(".container");
 
 async function games() {
+    try{
+        const respons = await fetch(url);
+
+        const results = await respons.json();
     
-        const response = await fetch(url);
-        const results = await response.json();
-        const name = results.all;
-        
-        console.log(results);
-        
-        resultContainer.innHTML ="";
-
-        for (let i = 0; i < name.lenght; i++) {
-            console.log(name[i].text);
-
+        const itemInfo = results.results;
+    
+        gamesections.innerHTML = "";
+    
+        for(let i = 0; i < itemInfo.length; i++) {
+            console.log(itemInfo[i].name);
+            console.log(itemInfo[i].rating);
+            console.log(itemInfo[i].tags.length);
+    
             if (i === 8){
                 break;
             }
+    
+            gamesections.innerHTML += `<div class="gamecontainer">
+            <p>Game: ${itemInfo[i].name} </p>
+            <p>Rating: ${itemInfo[i].rating} </p>
+            <p>Number of tags:${itemInfo[i].tags.length} </p>  
+            </div>`
+        }
 
-        resultsContainer.innerHTML += `<div class="results">${name[i].text}</div>`;
+    } catch(error) {
+        console.log("ohhh no something whent wrong");
+        gamesections.innerHTML = "ohhh no something whent wrong"
     }
-
 }
+
+
 games();
